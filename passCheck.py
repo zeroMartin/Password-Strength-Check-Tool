e0d93f0f047f0710ea6436419be999853d0f0a69
zeroMartin <brckovic.martin95@gmail.com>
2023-07-16 09:11:54 +0200

    strenght boundries changed, print statements finished

--- passCheck.py
+++ passCheck.py
@@ -1,7 +1,15 @@
 import time
 from termcolor import colored
 
+print('\n')
+print(colored('Password recommendation: ', 'blue'))
+print(' -> Have at least 12 characters in your password. Longer is always better')
+print(' -> For the best security, use upper and lower case letters, numbers and special characters')
+print(' -> Regardles of the password you use, always enable multi-factor authentication for any online account')
+print('\n')
 password = input('Enter password: ')
+print('_____________________________________________________')
+print('\n')
 
 U = 'U'
 L = 'L'
@@ -51,6 +59,8 @@ def cliTest():
     else:
         print('Password has more then 8 characters? ...', end = '')
         print(failed)
+    print('_____________________________________________________')
+    print('\n')
     time.sleep(0.5)
     return
 
@@ -77,104 +87,203 @@ def check_password_length():
     return password_length
 
 def check_strength():
-
     # Only numbers in password
     if N in character_type and U not in character_type and L not in character_type and S not in character_type:
-        print('Password consits only of numbers')
+        print('Password: ', password)
+        print(' -> Password consits only of numbers')
 
-        if password_length <= 15:
-            print('Your password is to weak and failed.')
+        if password_length < 16:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed.', 'red'))
         elif password_length == 16:
-            print('Password is weak')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one week')
+            print('\n')
+            print(colored('Password is weak and failed', 'red'))
         elif password_length >= 17 and password_length <= 18:
-            print('Password is intermediate.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is medium strenght', 'yellow'))
+        elif password_length >= 19 and password_length <= 20:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one year')
+            print('\n')
+            print(colored('Password is strong', 'green'))
         else:
-            print('Password is strong')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
 
     # Only upper case letters in password
     if N not in character_type and U in character_type and L not in character_type and S not in character_type:
-        print('Password consist only of upper case letters')
+        print('Password: ', password)
+        print(' -> Password consist only of upper case letters')
 
-        if password_length <= 10:
-            print('Your password is to weak and failed.')
-        elif password_length == 11:
-            print('Password is weak')
+        if password_length <= 11:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed.', 'red'))
         elif password_length == 12:
-            print('Password is intermediate.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is medium strenght', 'yellow'))
+        elif password_length == 13:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one year')
+            print('\n')
+            print(colored('Password is strong.', 'green'))
         else:
-            print('Password is strong')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
 
     # Only lower case letters in password
     if N not in character_type and U not in character_type and L in character_type and S not in character_type:
-        print('Password consist only of lower case letters')
+        print('Password: ', password)
+        print(' -> Password consist only of lower case letters')
 
-        if password_length <= 10:
-            print('Your password is to weak and failed.')
-        elif password_length == 11:
-            print('Password is weak')
+        if password_length <= 11:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed.', 'red'))
         elif password_length == 12:
-            print('Password is intermediate.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is medium strenght', 'yellow'))
+        elif password_length == 13:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one year')
+            print('\n')
+            print(colored('Password is strong.', 'green'))
         else:
-            print('Password is strong')
-
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
+        
     # Upper and lower case letters in password
     if N not in character_type and U in character_type and L in character_type and S not in character_type:
-        print('Password consist of upper and lower case letters')
+        print('Password: ', password)
+        print(' -> Password consist of upper and lower case letters')
 
         if password_length <= 9:
-            print('Your password is to weak and failed.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed', 'red'))
+        elif password_length == 10:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is medium strenght', 'yellow'))
         else:
-            print('Password is strong')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
 
     # Upper case letters and numbers in password
     if N in character_type and U in character_type and L not in character_type and S not in character_type:
-        print('Password consist of upper case letters and numbers')
+        print('Password: ', password)
+        print(' -> Password consist of upper case letters and numbers')
 
         if password_length <= 9:
-            print('Your password is to weak and failed.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed', 'red'))
+        elif password_length == 10:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is medium strenght', 'yellow'))
         else:
-            print('Password is strong')
-
-
-    # Lower case letters in password
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
+        
+    # Lower case letters and numbers in password
     if N in character_type and U not in character_type and L in character_type and S not in character_type:
-        print('Password consist of lower case letters and numbers')
+        print('Password: ', password)
+        print(' -> Password consist of lower case letters and numbers')
 
         if password_length <= 9:
-            print('Your password is to weak and failed.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed', 'red'))
+        elif password_length == 10:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is medium strenght', 'yellow'))
         else:
-            print('Password is strong')
-
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
+        
     # Numbers, Upper and lower case letters in password
     if N in character_type and U in character_type and L in character_type and S not in character_type:
-        print('Password consist of numbers, upper and lower case letters')
+        print('Password: ', password)
+        print(' -> Password consist of numbers, upper and lower case letters')
 
         if password_length <= 8:
-            print('Your password is to weak and failed.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed.', 'red'))
         elif password_length ==9:
-            print('Your password is weak')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one week')
+            print('\n')
+            print(colored('Password is weak and faild', 'red'))
         elif password_length == 10:
-            print('Your password is intermediate.')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one year')
+            print('\n')
+            print(colored('Password is strong.', 'green'))
         else:
-            print('Password is strong')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
 
     # Numbers, Upper and lower case letters and special characters in password
     if N in character_type and U in character_type and L in character_type and S in character_type:
-        print('Password consist of numbers, upper and lower case letters and special characters')
+        print('Password: ', password)
+        print(' -> Password consist of numbers, upper and lower case letters and special characters')
 
         if password_length <= 8:
-            print('Your password is to weak and failed.')
-        elif password_length ==9:
-            print('Your password is intermediate')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then 24h')
+            print('\n')
+            print(colored('Password is very weak and failed.', 'red'))
+        elif password_length == 9:
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Less then one month')
+            print('\n')
+            print(colored('Password is intermediate strenght', 'yellow'))
         else:
-            print('Password is strong')
+            print(' -> Password lenght: ', len(password))
+            print(' -> Estimated time to crack password: Couple of years')
+            print('\n')
+            print(colored('Password is very strong', 'green'))
     return
 
 character_type = check_character_type()
-#print(character_type)
 
 password_length = check_password_length()
-#print(password_length)
 
 cliTest()
 
